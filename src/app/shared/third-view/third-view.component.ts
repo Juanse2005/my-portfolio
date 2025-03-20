89f8f9afed0552f3a30bb73013716ff306b2c0b5
Juan Sebastian <juannino907@gmail.com>
2025-03-20 16:19:07 -0500

    [JN] - Added first view, second view and third view

---
+++ src/app/shared/third-view/third-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './third-view.component.html',
+  styleUrl: './third-view.component.css'
+})
+export class ThirdViewComponent {
+
+}
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NavComponent } from '../../shared/components/nav/nav.component';
+import { FirstViewComponent } from '../../shared/first-view/first-view.component';
+import { SecondViewComponent } from '../../shared/second-view/second-view.component';
+import { ThirdViewComponent } from '../../shared/third-view/third-view.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, FirstViewComponent, SecondViewComponent, ThirdViewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent {  
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollTop = window.scrollY; 
+    const cloud = document.getElementById('cloud-1');
 
+    if (cloud) {
+      cloud.style.transform = `translate(-50%, -${scrollTop * 0.5}px)`; // Se mueve más lento que el scroll
+    }
+  }
 }
